27dd4f0ad12b88b767acefc35ffe6996a7733ca2
Edward Martinez <emartinezj1802@gmail.com>
2026-02-04 19:24:34 -0500

    se crea tabla de propiedades basica pendiente incluir acciones

--- admin/index.php
+++ admin/index.php
@@ -17,6 +17,31 @@ incluirTemplate('header');
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <th>casa en la playa</th>
+                    <th><img src="/imagenes/8fe9b0324a38da8e4978c7e7dae5c313.jpg" class="imagen-tabla" alt=""></th>
+                    <th>$270.000.000</th>
+                    <th>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-verde-block">Actualizar</a>
+                    </th>
+                </tr>
+            </tbody>                
+        </table>
     </main>
 
 <?php
